01d0491701858c2842d18404cb357c5ae61b4b48
yoyo <a4253607@gmail.com>
2022-09-08 18:42:14 +0800

    Update 2022-09-08T10:42:11.883Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,15 +3,14 @@
 $(function () {
   console.log('Hello Bootstrap5');
 }); //swipper
-
-var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  centeredSlides: true,
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
-  }
-});
+// const swiper = new Swiper('.swiper-container', {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+// centeredSlides: true,
+// loop: true,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
 //# sourceMappingURL=all.js.map
