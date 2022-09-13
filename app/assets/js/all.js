97fb52faeef4d599a1f11eea02c920d409b0f8fb
yoyo <a4253607@gmail.com>
2022-09-13 17:33:20 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,16 +3,8 @@ $(function() {
 });
 
 
+$(".triangle-plus").click(function(e)
+{
+  $(this).toggleClass("triangle-minus")
+})
 
-
-//swipper
-// const swiper = new Swiper('.swiper-container', {
-//   slidesPerView: 3,
-//   spaceBetween: 30,
-// centeredSlides: true,
-// loop: true,
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-// });
